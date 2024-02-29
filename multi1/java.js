f18ad283519430c7d6b40d8fc2f3cd4746384dc4
arizsalman <muhammadarizsalman@gmail.com>
2024-02-29 17:29:10 +0500

    best

--- multi1/java.js
+++ multi1/java.js
@@ -10,4 +10,75 @@ var b=4
 
 var multiplication =15*4
 
+var c = 12 
+ var d = 3
+ var multiplication2=12**3
+
+ var e = 10 
+  var f = 3
+   var some=e +  f * 2
+
+   var g = 20 
+   var h = 4
+   var div= g / h + 5 + 4
+
+   var i = 8 
+   var j = 3
+
+   var mix= i * j-2 - i
+
+   var k = 18 
+    var l = 5
+    var mp= k - l + 2
+
+    var m = 7 
+    var n = 2
+    var mu= m ** n
+
+    var o = 13 
+     var p = 4
+      var per= o % (p + 2)
+
+      var q = 16 
+      var r = 3
+      var dm=  q * r  / q * 2
+
+
+      var s = 25
+       var t = 3
+       var ds= s / t- 1 - s
+
+       var u = 11 
+       var v = 3
+       var kk=u - v  + 2 +v
+
+       var w = 22 
+       var x = 5
+       var ans= 13* w + (x ** 2)
+
+       var y = 19 
+        var z = 4
+        var na = y % (z + 1)
+
+        var a=10
+        var b=3
+        var sub =a*(b-2)
+        
 console.log(multiplication)
+console.log(modulas)
+console.log(multiplication2)
+console.log(some)
+console.log(div)
+console.log(mix)
+console.log(mp)
+console.log(mu)
+console.log(per)
+console.log(dm)
+console.log(dm)
+console.log(ds)
+console.log(kk)
+console.log(na)
+console.log(sub)
+
+
+
