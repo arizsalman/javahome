62c32385f3a317b2f8bc41c116cbf21228edbad3
arizsalman <muhammadarizsalman@gmail.com>
2024-03-01 22:38:56 +0500

    kcp

---
+++ Concatenation/javacon.js
@@ -0,0 +1,25 @@
+ var fName= "John"
+ var lName= "Doe"
+ console.log(fName+lName)
+// var  name = `Salaam ${fName} ${lName}`
+
+var c='New'
+var l='York'
+console.log(c+l)
+
+var fr="Apple"
+var dr='pie'
+console.log(fr+dr)
+
+var ani='Black '
+var ni='Bird'
+console.log(ani+ni)
+
+var ja='JavaScript ' 
+var fu='IsFun'
+console.log(ja+fu)
+
+var fr= 'Intelligence '
+var op= 'open '
+var so= 'Source'
+console.log(fr+op+so)
---
+++ multi1/incre & decrement/java t.js
@@ -0,0 +1,28 @@
+var a=8
+console.log(a++)
+
+var b=15
+console.log(--b)
+
+var c=25
+console. log(c--)
+
+var d=10
+console.log(--d)
+
+var e=5
+console .log(e++)
+
+ var f=12
+ console.log(--f)
+
+ var g=18
+ console.log(g++)
+
+ var h=30
+ console.log(h--)
+  
+ var i=20
+ console.log(--i)
+ var j=8
+ console. log(j--)
