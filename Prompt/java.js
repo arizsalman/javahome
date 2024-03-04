cda33eb472d48d543041480b292ec9cfdcb509d7
arizsalman <muhammadarizsalman@gmail.com>
2024-03-05 00:58:19 +0500

    1to4 q  c

---
+++ Prompt/java.js
@@ -0,0 +1,76 @@
+//  var num = prompt( 'EnterTheNumber')
+//  if(num % 2==0 ){
+//     document.write(`<h3> ${num} is even.</h3>`)
+//  }else{
+//     document.write(`<h3>${num} is odd.</h3>`)
+//  }
+
+
+// var age=prompt(`EnterTheAge`)
+// if(age >= 18) {
+//     document.write(`<h1>You are adult.</h1>`)
+// }else{
+//     document.write(`<h1> You are not adult.</h1>`)
+// }
+
+//  var num=prompt('Input A Number')
+//  if(num >0){
+//     document.write(`<h1> The number is positive.</h1>`)
+//  } 
+//  else if(num<0)
+//     document.write(`<h1> The number is negative.</h1>`)
+ 
+//  else if(num=0)
+//     document.write( `<h1The number is Zero.</h1>`)
+ 
+
+// var firstNumber=prompt('Enter the second Number')
+
+// if(num>firstNumber){
+//     document.write(`<h1> The number islarge</h1>`)
+// }else{
+//     document.write(`<h1The number is Large.</h1>`)
+// }
+
+// var thirdNum=prompt('Enter The Third Number')
+// if(num >= 10 ){
+//     document.write(`<h1The number is Large.</h1>`)
+// }else{
+//     document.write(`<h1The number is Small.</h1>`)
+// }
+
+
+
+
+// var calNum=prompt('Solve Your Math Problem')
+// if(num+num){
+//     document.write(`Answer`)
+// }else{
+//     document.write(`Answer`)
+// }
+
+
+var num1=prompt('Enter First Number');
+var num2=prompt('Enter Second Number');
+if(num1>num2){
+    LargerNumber=num1;
+}else{
+    LargerNumber=num2;
+}
+console.log('The Larger number is',LargerNumber);
+
+var num1=prompt("Enter First Number");
+var num2=prompt("Enter Second Number");
+var num3=prompt('Enter Third Number');
+if(num1<num2<num3){
+    SmallerNumber=num1;
+}else{
+    SmallerNumber=num2
+}if (SmallerNumber=num3) {
+    
+}else {
+    SmallerNumber=num3
+}
+console.log("The Smaller number is" ,SmallerNumber);
+
+
