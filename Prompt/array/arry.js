ccb28990acb75dc3f41344015c221acb2e9449e9
arizsalman <muhammadarizsalman@gmail.com>
2024-03-14 23:44:59 +0500

    1 to 3

---
+++ Prompt/array/arry.js
@@ -0,0 +1,16 @@
+// Question no 1
+var studentName=[];
+console.log(studentName);
+
+//question no 2
+var studentObj=[]
+console.log(studentObj);
+
+// question no 3
+ var Names=[ 'Haris','Ali','Alyan','java' ];
+console.log(Names);
+  document.write(Names[0]+" Is Name.<br>");
+  document.write(Names[1]+" Is Name.<br>");
+  document.write(Names[2]+" Is Name.<br>"); 
+  document.write(Names[3]+" Is Not  Name.<br>"); 
+ 
