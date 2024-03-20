0b01888e3944a772df37c8747aaa67d1fdaeee05
arizsalman <muhammadarizsalman@gmail.com>
2024-03-21 00:33:01 +0500

    Arry and loop

--- Prompt/array/arry.js
+++ Prompt/array/arry.js
@@ -1,16 +1,134 @@
 // Question no 1
-var studentName=[];
-console.log(studentName);
+// var studentName=[];
+// console.log(studentName);
 
-//question no 2
-var studentObj=[]
-console.log(studentObj);
+// //question no 2
+// var studentObj=[]
+// console.log(studentObj);
 
-// question no 3
- var Names=[ 'Haris','Ali','Alyan','java' ];
-console.log(Names);
-  document.write(Names[0]+" Is Name.<br>");
-  document.write(Names[1]+" Is Name.<br>");
-  document.write(Names[2]+" Is Name.<br>"); 
-  document.write(Names[3]+" Is Not  Name.<br>"); 
+// // question no 3
+//  var Names=[ 'Haris','Ali','Alyan','java' ];
+// console.log(Names);
+//   document.write(Names[0]+" Is Name.<br>");
+//   document.write(Names[1]+" Is Name.<br>");
+//   document.write(Names[2]+" Is Name.<br>"); 
+//   document.write(Names[3]+" Is Not  Name.<br>"); 
+
+//   // question no 4
+//  var string=[ '1','2','3','4','5','6'];
+//  console.log(string);
+
+//  // question no 5
+//   var A=[ '10', '15'];
+//    console.log(A);
+
+
+// // question no 6
+//  var  mixed=[ 'Haris','12','Ali','33','Bilal','440'];
+//  console.log(mixed );
+
+//  // question no 7
+//   var  typ=[  'SSC', "HSC", 'BCS',' BS', 'BCOM', 'MS', 'M', 'Phil',' PhD']
+  
+// // document.write(cities[0] + " tan. <br>");
+//   document.write ( typ[0]+" . <br>" ,  );
+//   document.write ( typ[1]+" . <br>" ,  );
+//   document.write ( typ[2]+" . <br>" ,  );
+//   document.write ( typ[3]+" . <br>" ,  );
+//   document.write ( typ[4]+" . <br>" ,  );
+//   document.write ( typ[5]+" . <br>" ,  );
+//   document.write ( typ[6]+" . <br>" ,  );
+//   document.write ( typ[7]+" . <br>" ,  );
+//   document.write ( typ[8]+" . <br>" ,  );
+
+// // question no 8
+var score  =['  haris ',' Ali',' Ahmed' , 'Alyan' ,'Bilal'];
+var score1 =[422,478,486,390,444];
+ console.log(score[0] +  score1[0] / 500 * 100);
+ console.log( score[1]+ score1[1]/ 500* 100);
+ console.log(score[2]+ score1[2] /500 *100);
+ console.log(score[3]+ score1[3] /500 *100);
+ console.log(score[4]+ score1[4] /500 *100);
+//  console.log(score+ score1 .join( '&'));
+
+
+
+ // question no 17
+//  var far=('  haris ',' Ali',' Ahmed' , 'Alyan' ,'Bilal')
+//  var far1=(422,478,486,390,444)
+// // var far2=prompt( "Enter Your name")
+
+
+
+
+// questipon no 9
+//  var colour=['Blue','green', 'Red', 'yellow' ,' haris ',' Ali',' Ahmed' , 'Alyan' ,'Bilal'];
  
+//  console.log(colour[0]);
+//  console.log(colour[1]);
+//  console.log(colour[2]);
+//  console.log(colour[3]);
+//  console.log(colour[4]);
+//  console.log(colour[5]);
+//  console.log(colour[6]);
+//  console.log(colour[7]);
+//  console.log(colour[8]);
+//  console.log(colour.toString ());
+//  console.log(colour.join (" & "));
+// colour.push(+8)
+// console.log(colour);
+
+
+  //  var col=['red','Blue','green', 'Red', 'yellow']
+  //  var ycol=prompt(' he/she wants to add to thebeginning & add that color')
+  // // var col=[eerj,em,ekj,lkl,sd,];
+  //  col.push('gj')
+  //  console.log(col);
+  //  console.log(col.join(" & "));
+   //question no B 
+  //  var colo=['red','Blue','green', 'Red', 'yellow']
+  //  var colA=prompt('Best colour');
+  //  indexOfColr=colo.indexOf(colA)
+  //  if (indexOfColr === -1) {
+  //     colo.push(colA);    
+  //  }else{
+  //   alert('Alredy Added')
+  //  }
+
+  //  var colA1=prompt('Best colour');
+  //  indexOfColr=colo.indexOf(colA1)
+  //  if (indexOfColr === -1) {
+  //     colo.push(colA1);    
+  //  }else{
+  //   alert('Alredy Added')
+  //  }
+  //  console.log(colo);
+
+  // var fd=['1','2','23']
+  // fd.splice(2,1,878)
+  // console.log(fd);
+
+
+  //question no 11
+
+   var pet=['This', 'is' ,'my', 'Cat', ]
+   console.log(pet);
+   console.log(pet.toString( ));
+   console.log(pet.join( " " ));
+
+   //question no 12
+   var totalColour=[ 'red','green','blue','brown','grey','yellow',]
+   console.log( totalColour );
+   console.log( totalColour.toString());
+document.write( totalColour[0]+' .  <h1> <br>');
+document.write( totalColour[1]+' .  <h1> <br>');
+document.write( totalColour[2]+' .  <h1> <br>');
+document.write( totalColour[3]+' .  <h1> <br>');
+document.write( totalColour[4]+' .  <h1> <br>');
+document.write( totalColour[5]+' .  <h1> <br>');
+totalColour.splice(0,1,'pink');
+console.log(totalColour);
+totalColour[1]="pink"
+console.log(totalColour);
+
+
---
+++ multi1/For Loop/loop.js
@@ -0,0 +1,51 @@
+// //question no 1
+for (let i = 1; i <=10; i++) {
+    console.log(i);
+}
+
+// //question no 2
+for (let i =2; i<= 10; i++) {
+console.log(i+ '  is  ' + i * i );
+}
+
+// //question no 3
+var fruits = ['apple', 'banana', 'mango', 'orange', ' strawberry'];
+for (let i = 0; i< fruits.length; i++) {
+    console.log(  fruits[i]); 
+}
+for( let i=0;i<fruits.length;i++ ){
+    console.log('  Element At Index  ' +i+' is ' +fruits[i]);
+}
+    
+//question no 4
+for (let i = 1; i <=20 ;i++) {
+    if ( i %  2 == 0) {
+        
+        console.log(i);
+    }
+}
+// //question no5
+for (let i = 1; i <= 15; i++) {
+      if (i % 2 == 1) {
+        console.log(i);
+      }
+    }
+    
+// question no 6
+ 
+
+for (let i = 5;  i<=10 ; i++) {
+    // i * i;
+    console.log(i*i);
+}
+
+
+// question no 7
+ for (let i = '1'; i <=10; i++) {
+console.log(' Hello '+i);
+ }
+
+// question no 8
+for (let i =[ 3, 7, 2, 9, 5 ]; i >=  i; i++) {
+    console.log(i);
+}
