263d114ab2276081ccd90c74b87d17a17dba849b
arizsalman <muhammadarizsalman@gmail.com>
2024-04-18 00:36:37 +0500

    complete

--- Changing Case/java.js
+++ Changing Case/java.js
@@ -1,2 +1,86 @@
-var lastName=['Hello my name is haris salman .']
-console.log(lastName.slice(0,5));
+// var lastName='Hello my name is haris salman .';    
+// lastName.toLowerCase();
+// console.log(lastName);
+
+//  var username = "BaSit";
+
+//  username.toLowerCase();
+
+// console.log(username);
+// //question no 1
+// var a='asalam o alaikum';
+// for (let i = 0; i < a.length; i++) {
+    // console.log(a);    
+    // }
+    
+    // //question no 1 part 2
+
+    var b='my name is haris salman & i was born in karachi';
+    for (let i = 0; i < b.length; i++) {
+        console.log(b);
+        
+ }
+
+
+// //question no2
+
+// // console.log(UpperCase.charAt());
+// // var U='Hello! my name is$ haris$ salman$# .';
+// for (var i = 0; i<Text.length; i++){
+//     if (Text.chartAt(i)==="!"){ 
+//         alert('Exclamation point foun!');
+//         break;
+//     }
+// }
+
+// question no 3  upper case
+var u='KaRachi is big city';
+console.log(u.toUpperCase());
+
+// question no 4  lower case
+var lo="MY NAmE Is hARIS sAlMaN.";
+console.log(lo.toLowerCase());
+
+
+// question no 5  Specific Character.
+var sp='Hello World!';
+console.log(sp.charAt(11));
+
+
+// question no 6  Sub Method.
+
+var su="my name is haris salman.";
+console.log(su.substring(10));
+
+
+// question no 6   Capitalize  Method.
+
+// var cap="my name is haris salman.";
+// console.log(cap.);
+
+
+// question no 7   Trim  Method.
+
+var tri='   muhammad ariz salman @gmail . com ';
+console.log(tri.length);
+console.log(tri.trim().length);
+
+
+// question no 8   Count Method.
+var Count=["a",'e','i','o','u'];
+console.log(Count.length);
+
+// question no 9   Index Method.
+var ind ='muhammad ariz salman @gmail . com my name is  ariz salman. '
+console.log(ind.indexOf('@'));
+
+
+// question no 10  Remove  duplicate Method.
+
+var rd='muhammad ariz salman @gmail . com my name is  ariz salman. '
+console.log(rd.replace('@'));
+
+
+
+
+
