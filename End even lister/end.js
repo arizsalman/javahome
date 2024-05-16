a72bfdfc43444775578da58c4fbc1b6b69783afb
arizsalman <muhammadarizsalman@gmail.com>
2024-05-16 17:45:17 +0500

    ok

--- End even lister/end.js
+++ End even lister/end.js
@@ -2,18 +2,17 @@ var btn=document.getElementById("bulbon");
 var btn1=document.getElementById("bulboff");
 
 btn.addEventListener('click', function () {
-  if (btn.classList.contains('on')) {
-    btn.classList.remove('on');
-  }else{
-    btn.classList.add('on')
-  }
+  btn.classList.toggle('on')
+  btn1.classList.toggle('on')
+  btn.innerText = "Turned On"
+  btn1.innerText = "Turned On"
 })
 
-btn1.addEventListener('click',function () {
-    if (btn1.classList.contains('on')) {
-        btn1.classList.remove('on');
-    }else{
-        btn1.classList.add('on')
+// btn1.addEventListener('click',function () {
+//     if (btn1.classList.contains('on')) {
+//         btn1.classList.remove('on');
+//     }else{
+//         btn1.classList.add('on')
 
-    }
-})
+//     }
+// })
