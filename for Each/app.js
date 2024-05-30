58aad4b471b9596cf570b3452d5ea80aa1400efa
arizsalman <muhammadarizsalman@gmail.com>
2024-05-30 17:34:06 +0500

    ok

--- for Each/app.js
+++ for Each/app.js
@@ -63,12 +63,40 @@ fruits.forEach(function (data, ind) {
       <p class="leading-relaxed">${data.types.join(" , ")}</p>
       <p class="leading-relaxed">${data.desc} </p>
       <button onclick="updateDesc(this)">update</button>
+      <button onclick="del(this)">Delete</button
     </div>
   </div>
 </div>`;
   list.innerHTML += ele;
 });
-
+function del(ele) {
+    var index=ele.parentElement.id;
+    fruits.splice(index,1)
+    console.log(fruits);
+    list.innerHTML=""
+    fruits.forEach(function (data, ind) {
+      var ele = `<div class="lg:w-1/3  sm:w-1/2 p-4">
+      <div class="flex relative h-[250px]">
+        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="${
+          data.image
+        }">
+        <div id = ${ind} class="px-8 py-10  relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <h2 class="tracking-widest text-xl title-font font-medium text-indigo-500 mb-1">${
+            data.name
+          }</h2>
+          <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">${
+            data.title
+          }</h1>
+          <p class="leading-relaxed">${data.types.join(" , ")}</p>
+          <p class="leading-relaxed">${data.desc} </p>
+          <button onclick="updateDesc(this)">update</button>
+          <button onclick="del(this)">Delete</button>
+        </div>
+      </div>
+    </div>`;
+      list.innerHTML += ele;
+    });
+  }
 function updateDesc(ele) {
   console.log("ele=>", ele.previousElementSibling);
   console.log("ele parentr=>", ele.parentElement);
@@ -93,6 +121,7 @@ function updateDesc(ele) {
         <p class="leading-relaxed">${data.types.join(" , ")}</p>
         <p class="leading-relaxed">${data.desc} </p>
         <button onclick="updateDesc(this)">update</button>
+        <button onclick="del()">Delete</button>
       </div>
     </div>
   </div>`;
@@ -101,7 +130,7 @@ function updateDesc(ele) {
 
 }
 
-// }
+
 
 
 
